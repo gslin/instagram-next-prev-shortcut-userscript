73ab99965fcb8705232827c2929229ccdd66bd74
Gea-Suan Lin <gslin@gslin.org>
2023-06-08 05:50:46 +0800

    Limit the button to div[role="dialog"] case.

--- instagram-next-prev-shortcut-userscript.user.js
+++ instagram-next-prev-shortcut-userscript.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/instagram-next-prev-shortcut-userscript
 // @match       https://www.instagram.com/*
 // @grant       none
-// @version     0.20230601.0
+// @version     0.20230608.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Add shortcuts for Instagram Next/Prev buttons
 // @license     MIT
@@ -16,13 +16,13 @@
     }
 
     if ('<' === event.key) {
-      const el = document.querySelector('button[aria-label="Go Back"]');
+      const el = document.querySelector('div[role="dialog"] button[aria-label="Go Back"]');
       el?.click();
       return;
     }
 
     if ('>' === event.key) {
-      const el = document.querySelector('button[aria-label="Next"]');
+      const el = document.querySelector('div[role="dialog"] button[aria-label="Next"]');
       el?.click();
       return;
     }
